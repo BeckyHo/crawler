57675839a7f1ac238caf110a5436ba473b8c01cb
yangguangyong <yangguangyong1015@gmail.com>
2016-07-27 18:05:06 +0800

    content changes

---
+++ web/src/test/java/com/crawler/test/SpringTest.java
@@ -0,0 +1,28 @@
+package com.crawler.test;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringTest {
+
+	private ApplicationContext ctx = null;
+
+	{
+		ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+	}
+
+	@Test
+	public void testConfig() {
+		DataSource dataSource = (DataSource) ctx.getBean("dataSource");
+		try {
+			System.out.println(dataSource.getConnection());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
